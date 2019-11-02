9eeb548d03e147b470fa9c35981195586ab32ab1
Lopè Ariyo <lopeariyo@gmail.com>
2019-11-02 08:30:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,9 +24,9 @@ def monopoly_with_third_tier
 		:pieces => 4,
 		:rent_in_dollars => {
 			:one_piece_owned => 25,
-			:key_two => "{}",
-			:key_three => "{}",
-			:key_four => "{}"},
+			:two_pieces_owned => 50,
+			:three_pieces_owned => 75,
+			:four_pieces_owned => 100},
 		:names => {
 			:key_one => "{}",
 			:key_two => "{}",
